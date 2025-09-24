ae284c80b98cb003e50a48867da3e62f67c37aa7
rodr1to <rovalverdea@gmail.com>
2025-09-24 13:44:18 -0500

    Initial Commit

---
+++ 4576-ecommerce-project/_576_ecommerce_projectApp.swift
@@ -0,0 +1,17 @@
+//
+//  _576_ecommerce_projectApp.swift
+//  4576-ecommerce-project
+//
+//  Created by Rodrigo Valverde on 9/24/2025.
+//
+
+import SwiftUI
+
+@main
+struct _576_ecommerce_projectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
