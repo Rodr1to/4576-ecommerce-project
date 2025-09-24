084b5d5f2a7208dcc88c9dbc743f6c41f510e748
rodr1to <rovalverdea@gmail.com>
2025-09-24 14:43:47 -0500

    1st commit: updates and HomeView + IntroView screens

---
+++ 4576-ecommerce-project/HomeView.swift
@@ -0,0 +1,18 @@
+//
+//  HomeView.swift
+//  4576-ecommerce-project
+//
+//  Created by Rodrigo Valverde on 9/24/2025.
+//
+
+import SwiftUI
+
+struct HomeView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    HomeView()
+}
